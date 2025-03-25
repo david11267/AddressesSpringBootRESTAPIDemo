36351a068a406135913d77a173cc3c6f8ec3cb04
David Aslan <davidaslan112671@live.se>
2025-03-25 12:12:57 +0100

    INIT

---
+++ src/test/java/com/example/addressApi/AddressApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.addressApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
